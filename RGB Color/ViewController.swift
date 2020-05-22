f22adbb39a8c5cec4efd3ef4ae8fd0fba1ee6a37
Vitalii Sosin <375693@mail.ru>
2020-05-22 21:51:13 +0300

    Optimized the application

--- RGB Color/ViewController.swift
+++ RGB Color/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSliderOutlet: UISlider!
     @IBOutlet weak var greenSliderOutlet: UISlider!
     @IBOutlet weak var blueSliderOutlet: UISlider!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,29 +27,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sliderAction() {
-        
-        var color: UIColor {
-            return UIColor(
-                red: CGFloat(redSliderOutlet.value),
-                green: CGFloat(greenSliderOutlet.value),
-                blue: CGFloat(blueSliderOutlet.value),
-                alpha: 1
-            )
-        }
-        
-        screenView.backgroundColor = color
-        
-        redValueLabel.text = String(format: "%.2f", redSliderOutlet.value)
-        greenValueLabel.text = String(format: "%.2f", greenSliderOutlet.value)
-        blueValueLabel.text = String(format: "%.2f", blueSliderOutlet.value)
+        getsColor()
+        roundingValues()
     }
     
+    // MARK: - Default setting viewDidLoad
     private func setDefaultSetting() {
         redValueLabel.text = String(redSliderOutlet.value)
         greenValueLabel.text = String(greenSliderOutlet.value)
         blueValueLabel.text = String(blueSliderOutlet.value)
         
-//        screenView.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
-        screenView.layer.cornerRadius = 20
+        screenView.layer.cornerRadius = screenView.frame.width / 15
+    }
+    
+    // MARK: - Gets color from RGB
+    private func getsColor() {
+        let color = UIColor(
+            red: CGFloat(redSliderOutlet.value),
+            green: CGFloat(greenSliderOutlet.value),
+            blue: CGFloat(blueSliderOutlet.value),
+            alpha: 1
+        )
+        screenView.backgroundColor = color
+    }
+    
+    // MARK: - Rounding values
+    private func roundingValues() {
+        redValueLabel.text = String(format: "%.2f", redSliderOutlet.value)
+        greenValueLabel.text = String(format: "%.2f", greenSliderOutlet.value)
+        blueValueLabel.text = String(format: "%.2f", blueSliderOutlet.value)
     }
 }
