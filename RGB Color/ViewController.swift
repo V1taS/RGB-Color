e1e401e8afe594db43f8e26af37a3657ae7a8d4f
Vitalii Sosin <375693@mail.ru>
2020-05-22 21:28:21 +0300

    Made a system dark theme

--- RGB Color/ViewController.swift
+++ RGB Color/ViewController.swift
@@ -19,14 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSliderOutlet: UISlider!
     @IBOutlet weak var greenSliderOutlet: UISlider!
     @IBOutlet weak var blueSliderOutlet: UISlider!
-    
-    @IBOutlet weak var switchOutlet: UISwitch!
-    
-    @IBOutlet weak var redNamelabel: UILabel!
-    @IBOutlet weak var greenNamelabel: UILabel!
-    @IBOutlet weak var blueNamelabel: UILabel!
-    @IBOutlet weak var darkModelabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,9 +49,7 @@ class ViewController: UIViewController {
         greenValueLabel.text = String(greenSliderOutlet.value)
         blueValueLabel.text = String(blueSliderOutlet.value)
         
-        switchOutlet.layer.cornerRadius = 15
-        
-        screenView.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+//        screenView.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
         screenView.layer.cornerRadius = 20
     }
 }
