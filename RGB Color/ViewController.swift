cc9a09b45876b9fabac1b49b5445f3d5f348d8d6
Vitalii Sosin <375693@mail.ru>
2020-05-22 06:42:00 +0300

    Made an application

--- RGB Color/ViewController.swift
+++ RGB Color/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var screenView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSliderOutlet: UISlider!
+    @IBOutlet weak var greenSliderOutlet: UISlider!
+    @IBOutlet weak var blueSliderOutlet: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setDefaultSetting()
     }
-
-
+    
+    @IBAction func sliderAction() {
+        
+        var color: UIColor {
+            return UIColor(
+                red: CGFloat(redSliderOutlet.value),
+                green: CGFloat(greenSliderOutlet.value),
+                blue: CGFloat(blueSliderOutlet.value),
+                alpha: 1
+            )
+        }
+        
+        screenView.backgroundColor = color
+        
+        redValueLabel.text = String(format: "%.2f", redSliderOutlet.value)
+        greenValueLabel.text = String(format: "%.2f", greenSliderOutlet.value)
+        blueValueLabel.text = String(format: "%.2f", blueSliderOutlet.value)
+    }
+    
+    func setDefaultSetting() {
+        redValueLabel.text = String(redSliderOutlet.value)
+        greenValueLabel.text = String(greenSliderOutlet.value)
+        blueValueLabel.text = String(blueSliderOutlet.value)
+        screenView.backgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+    }
 }
-
