cebbeda2913a5e6a1a84e5b988d8c1ffb0d68e6f
Vitalii Sosin <375693@mail.ru>
2020-05-22 08:40:28 +0300

    Create dark mode

--- RGB Color/ViewController.swift
+++ RGB Color/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSliderOutlet: UISlider!
     @IBOutlet weak var blueSliderOutlet: UISlider!
     
+    @IBOutlet weak var switchOutlet: UISwitch!
+    
+    @IBOutlet weak var redNamelabel: UILabel!
+    @IBOutlet weak var greenNamelabel: UILabel!
+    @IBOutlet weak var blueNamelabel: UILabel!
+    @IBOutlet weak var darkModelabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,11 +51,57 @@ class ViewController: UIViewController {
         blueValueLabel.text = String(format: "%.2f", blueSliderOutlet.value)
     }
     
+    @IBAction func switchAction() {
+        
+        if switchOutlet.isOn {
+            let navigationBar = self.navigationController?.navigationBar
+            navigationBar?.barStyle = UIBarStyle.black
+//            navigationBar?.tintColor = UIColor.black
+            view.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            
+            redNamelabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            greenNamelabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            blueNamelabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            darkModelabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            
+            redValueLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            greenValueLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            blueValueLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            
+            redSliderOutlet.maximumTrackTintColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            greenSliderOutlet.maximumTrackTintColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            blueSliderOutlet.maximumTrackTintColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            
+        } else {
+            let navigationBar = self.navigationController?.navigationBar
+            navigationBar?.barStyle = UIBarStyle.default
+//            navigationBar?.tintColor = UIColor.white
+            view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            
+            redNamelabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            greenNamelabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            blueNamelabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            darkModelabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            
+            redValueLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            greenValueLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            blueValueLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            
+            redSliderOutlet.maximumTrackTintColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            greenSliderOutlet.maximumTrackTintColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            blueSliderOutlet.maximumTrackTintColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            
+        }
+    }
+    
     private func setDefaultSetting() {
         redValueLabel.text = String(redSliderOutlet.value)
         greenValueLabel.text = String(greenSliderOutlet.value)
         blueValueLabel.text = String(blueSliderOutlet.value)
         
-        screenView.backgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        switchOutlet.layer.cornerRadius = 15
+        
+        screenView.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+        screenView.layer.cornerRadius = 20
     }
 }
